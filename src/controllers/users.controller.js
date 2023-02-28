13978473243ecbbc75e284a16ddb5d2cccb7d6a5
luanmed <luanmede@gmail.com>
2023-02-28 10:29:01 -0300

    fix: get users/me

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -4,14 +4,15 @@ export async function getUsersMe(req, res) {
     const userId = res.locals.user.user_id;
 
     try {
-        // const user = await db.query(`SELECT * FROM users WHERE id=$1;`, [userId]);
-        // const visitCount = await db.query(`SELECT links."visitCount" FROM links WHERE ;`);
-
         const body = await db.query(`
-        SELECT users.id, users.name,
-        SUM(links."visitCount") AS visitCount,
-        json_agg(links) AS "shotenedUrls"
-        FROM users INNER JOIN links ON links.user_id = users.id
+        SELECT users.id, users.name, SUM(links."visitCount") AS "visitCount",
+        json_agg(json_build_object(
+	        'id', links.id,
+	        'shorUrl', links."shortUrl",
+	        'url', links.url,
+	        'visitCount', links."visitCount"
+        )ORDER BY links.id) AS "shotenedUrls"
+        FROM users JOIN links ON links.user_id = users.id
         WHERE users.id = $1
         GROUP BY users.id, users.name;
         `, [userId]);
