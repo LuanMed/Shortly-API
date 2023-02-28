35c8b8637667a59a08416eb7fc757600984937ea
luanmed <luanmede@gmail.com>
2023-02-28 14:27:38 -0300

    fix: ranking not in the right order

--- src/controllers/ranking.controller.js
+++ src/controllers/ranking.controller.js
@@ -5,7 +5,7 @@ export async function visitRanking (req, res) {
         const body = await db.query(`
         SELECT users.id, users.name, COUNT(links.id) AS "linksCount", SUM(links."visitCount") AS "visitCount"
         FROM users LEFT JOIN links ON links.user_id = users.id
-        GROUP BY users.id ORDER BY "visitCount" LIMIT 10;`);
+        GROUP BY users.id ORDER BY "visitCount" DESC LIMIT 10;`);
 
         res.send(body.rows);
     } catch (error) {
